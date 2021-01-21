ea5ee51fccf1f79a3005a605fdeb3adeeeba689e
Asma Al-Odaini <asmaalodaini@gmail.com>
2021-01-21 10:11:22 -0500

    Add example to preprocess.py script

--- src/preprocess.py
+++ src/preprocess.py
@@ -11,6 +11,8 @@ Options:
 --datafile=<datafile>          path to data file
 --out=<output_directory>       directory for saving processed file
 
+Example:
+    python src/preprocess.py --datafile=data/raw/lab2-movies.json --out=data/processed
 """
 from docopt import docopt
 import pandas as pd
@@ -18,33 +20,43 @@ import os
 
 opt = docopt(__doc__)
 
+
 def main(datafile, out):
     # load in the data
     movies = pd.read_json(datafile)
 
     # scale financials to millions
-    movies.budget = movies.budget/1000000
-    movies.revenue = movies.revenue/1000000
+    movies.budget = movies.budget / 1000000
+    movies.revenue = movies.revenue / 1000000
 
     # spot corrections in financials for two movies
-    movies.loc[movies.id == 15417, 'budget'] = movies.loc[movies.id == 15417, 'budget'] * 1000000
-    movies.loc[movies.id == 15417, 'revenue'] = movies.loc[movies.id == 15417, 'revenue'] * 1000000
+    movies.loc[movies.id == 15417, "budget"] = (
+        movies.loc[movies.id == 15417, "budget"] * 1000000
+    )
+    movies.loc[movies.id == 15417, "revenue"] = (
+        movies.loc[movies.id == 15417, "revenue"] * 1000000
+    )
 
-    movies.loc[movies.id == 16340, 'budget'] = movies.loc[movies.id == 16340, 'budget'] * 1000000
-    movies.loc[movies.id == 16340, 'revenue'] = movies.loc[movies.id == 16340, 'revenue'] * 1000000
+    movies.loc[movies.id == 16340, "budget"] = (
+        movies.loc[movies.id == 16340, "budget"] * 1000000
+    )
+    movies.loc[movies.id == 16340, "revenue"] = (
+        movies.loc[movies.id == 16340, "revenue"] * 1000000
+    )
 
-    # explode 
-    movies = movies.explode('studios').explode('genres')
+    # explode
+    movies = movies.explode("studios").explode("genres")
 
     # calculate new financial columns
-    movies['profit'] = movies.revenue - movies.budget
-    movies['profit_margin'] = movies.profit / movies.revenue
+    movies["profit"] = movies.revenue - movies.budget
+    movies["profit_margin"] = movies.profit / movies.revenue
 
     # save new file
     if not os.path.exists(out):
         os.makedirs(out)
 
-    movies.to_csv(f'{out}/movies.csv', index=False)
+    movies.to_csv(f"{out}/movies.csv", index=False)
 
-if __name__ == '__main__':
-    main(opt['--datafile'], opt['--out'])
+
+if __name__ == "__main__":
+    main(opt["--datafile"], opt["--out"])
